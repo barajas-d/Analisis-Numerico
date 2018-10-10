c6efaf038feaace01efcc43745e3993e6fde630b
Barajas-d <Diegobarajas102030@gmail.com>
2018-10-09 21:33:27 -0500

    Copo de nieve

--- Longitud de curva copo de nieve fractal.R
+++ Longitud de curva copo de nieve fractal.R
@@ -16,3 +16,11 @@ iteraciones = 2460
 perimetro = 9
 valores = LongiFractal(perimetro, iteraciones)
 print(valores[iteraciones])
+
+
+
+x = c(-2, 0, 2, 3, 4, 6, 7, 5, 8, 9, 10, 13, 11, 12, 14, 15, 16, 18, 20)
+y = c(1, 1, 1, 3, 1, 1, 3, 4.5, 5, 7, 5, 4.5, 3, 1, 1, 3, 1, 1, 1)
+plot(x,y,type="l",bty="L",xlab="X",ylab="Y")
+points(x,y,type="o",col="Red")
+
