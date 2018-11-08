1a8d23cbf682d702ede1ee31df0bc3d78a3f7608
Barajas-d <Diegobarajas102030@gmail.com>
2018-11-07 19:21:58 -0500

    Proyecto correcciones 2

---
+++ Proyecto/Richardson entrega final/Richardson/R/richardson.R
@@ -0,0 +1,59 @@
+derivada = function(x,y,h = 0.01,n = 3){
+  require(PolynomF)
+  lagrange = function(x,y,a){
+    n = length(x)
+    if(a < min(x) || max(x) < a) stop("No est? interpolando")
+    X = matrix(rep(x, times=n), n, n, byrow=T)
+    mN = a - X; diag(mN) = 1
+    mD = X - t(X); diag(mD) = 1
+    Lnk = apply(mN, 1, prod)/apply(mD, 2, prod)
+    sum(y*Lnk)
+  }
+
+  cinco2 = function(x,y,x0,h){
+    return((1/(2*h))*(interpolacion(x,y,(x0+h))-interpolacion(x,y,(x0-h))))
+  }
+
+  interpolacion = function(x,y,x0){
+    return(lagrange(x,y,x0))
+  }
+
+  richardsonH = function(x,y,x0,h,n){
+    if(n == 1){
+      return(c(cinco2(x,y,x0,h),0))
+    }else{
+      n1 = richardsonH(x,y,x0,(h/2),n-1)
+      n0 = richardsonH(x,y,x0,h,n-1)
+      r = n1[1]+((n1[1]-n0[1])/(4^(n-1)-1))
+      e = (abs(n1[1]-n0[1]))/(4^(n-1)-1)
+    }
+    return(c(r,e))
+  }
+
+  if(n > 15){
+    n = 15
+    cat("El numero de iteraciones es muy grande, se disminuir?n a ",n,"\n")
+  }
+  if(h >= x[length(x)] || h <= 0){
+    stop("h invalido")
+  }
+
+  minimo = 1
+  maximo = length(x)
+  for(i in 1:length(x)){
+    if((x[i]-h)>=x[1]){
+      minimo = i
+      break
+    }
+  }
+  for(i in 1:length(x)){
+    if((x[length(x)-i+1]+h)<=x[length(x)]){
+      maximo = length(x)-i+1
+      break
+    }
+  }
+  cat("x\t\ty\t\tR\t\te\n")
+  for(i in minimo:maximo){
+    cat(x[i],"\t",y[i],"\t",richardsonH(x,y,x[i],h,n),"\n")
+  }
+}
---
+++ Proyecto/Richardson entrega final/Richardson/R/richardson2.R
@@ -0,0 +1,116 @@
+integral <- function(x, y, maxParticiones = TRUE, particiones = 3){
+
+
+  maxTrape <- function(tam){
+    if(tam <= 2){
+      return (1)
+    }
+    else{
+      if((tam %% 2) == 0){
+        paso = (tam/2)
+      }
+      else{
+        paso = ((tam+1)/2)
+      }
+      return(1 + maxTrape(paso))
+    }
+  }
+
+  pasoPivote <- function(cantiParticiones, tamDatos){
+    while(cantiParticiones>1){
+      if((tamDatos %% 2) == 0){
+        tamDatos = (tamDatos/2)
+      }
+      else{
+        tamDatos = ((tamDatos+1)/2)
+      }
+      cantiParticiones = cantiParticiones - 1
+    }
+    return(tamDatos-1)
+  }
+
+  trapecio <- function(x1, y1, x2, y2){
+    h = x2-x1
+    area = (h/2)*((y1+y2))
+    return(area)
+  }
+
+  integralApro <- function (nivel, tam, x, y){
+    total = 0
+    paso = pasoPivote(nivel, tam)
+    trap = seq(1, tam, by = paso)
+    anterior = -1
+    for (i in trap){
+      if(anterior == -1){
+        anterior = i
+      }
+      else{
+        total = total + trapecio(x[anterior], y[anterior], x[i], y[i])
+        anterior = i
+      }
+    }
+    return(total)
+  }
+
+
+  if(length(x) != length(y)){
+    stop("Los valores para 'Y' no coinciden con los valores en 'X'")
+  } else if(length(x) < 3){
+    stop("Se nesesitan mas valores para hallar la integral")
+  } else{
+
+    if(maxParticiones == TRUE){
+      particiones = maxTrape(length(x))
+    }
+
+    inte = seq(1, particiones, by=1)
+    error = seq(1, particiones, by=1)
+    AuxError1 = 1
+    for (i in 1:particiones) {
+      inte[i] = integralApro(i, length(x), x, y)
+
+
+
+
+      if(i == 1){
+        AuxError1 = abs(inte[i] - 0)
+        error[i] = AuxError1/1
+      }
+      else{
+        AuxError1 = abs(inte[i] - inte[i-1])
+        if(i == 2){
+          aux = abs(((1/error[i-1])^i)-1)
+          if(is.na(aux)){
+            aux = 1
+          }
+          error[i] = AuxError1/(aux)
+        }
+        else{
+          aux = abs(((error[i-2]/error[i-1])^i)-1)
+          if(is.na(aux)){
+            aux = 1
+          }
+          error[i] = AuxError1/(aux)
+        }
+      }
+
+
+
+    }
+  }
+
+  canti = particiones
+  nivel = 2
+  while(canti > 1){
+    for (i in 1:canti-1) {
+      inte[i] = (((4^(nivel-1))/((4^(nivel-1))-1))*inte[i+1])-((1/((4^(nivel-1))-1))*(inte[i]))
+    }
+    canti = canti -1
+    nivel = nivel +1
+  }
+
+  cat("Integral definida de ", x[1], " a ", x[length(x)], " de los puntos dados es de ", inte[1], " con un error aproximado de ", abs(error[length(inte)]), "\n")
+  return(inte[1])
+
+}
+
